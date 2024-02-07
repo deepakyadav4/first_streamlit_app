05372818bd36e416529e833fbaef881a55b53819
deepakyadav4 <53937893+deepakyadav4@users.noreply.github.com>
2024-02-07 15:23:53 +0530

    Update streamlit_app.py
    loaded from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avacado Toast')
+
+import pandas
+my_fruit_list = pandas.read.csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
 
