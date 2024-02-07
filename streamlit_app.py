6a1d078936ccc88c008440085ee6fce8233da89e
deepakyadav4 <53937893+deepakyadav4@users.noreply.github.com>
2024-02-07 15:08:41 +0530

    Update streamlit_app.py
    Added Menu to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 and Blueberry Oat Meeal' )
+streamlit.text('Kale, Spinach, smoothie' )
+streamlit.text('Hard-Boiled Free Range Egg' )
